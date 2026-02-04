ca4bf8c5cb9cd3c20294da932aa0568521e7841c
ashishsmakani-ux <ashishsmakani@gmail.com>
2026-02-05 00:51:43 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab1Page } from './tab1.page';
+import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    Tab1PageRoutingModule,
+    Tab1Page // Standalone component ને અહીં import માં રાખવું
+  ],
+  declarations: [] // અહીંથી Tab1Page કાઢી નાખ્યું છે જેથી એરર ન આવે
 })
-export class AppModule {}
+export class Tab1PageModule {}
